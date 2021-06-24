31a3361d3ca5961d0aa0b8c4ea2d8968323ccdb2
ZinedineMess <zinedinemessahel@hotmail.com>
2021-06-24 11:27:14 +0200

    test : container/Bills statements

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,21 +1,29 @@
 import {
-  screen
-} from "@testing-library/dom";
-import BillsUI from "../views/BillsUI.js";
+  screen,
+  fireEvent
+} from '@testing-library/dom';
+import {
+  localStorageMock
+} from '../__mocks__/localStorage.js';
+import BillsUI from '../views/BillsUI.js';
 import {
   bills
-} from "../fixtures/bills.js";
+} from '../fixtures/bills.js';
+import {
+  ROUTES,
+} from '../constants/routes';
+import Bills from '../containers/Bills.js';
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
+describe('Given I am connected as an employee', () => {
+  describe('When I am on Bills Page', () => {
+    test('Then bill icon in vertical layout should be highlighted', () => {
       const html = BillsUI({
         data: []
       });
       document.body.innerHTML = html;
       //to-do write expect expression
     });
-    test("Then bills should be ordered from earliest to latest", () => {
+    test('Then bills should be ordered from earliest to latest', () => {
       const html = BillsUI({
         data: bills
       });
@@ -30,25 +38,119 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted);
     });
   });
+
+  // LOADING PAGE for views/BillsUI.js
   describe("When I am on Bills page but it's loading", () => {
-    test("Then I should land on a loading page", () => {
+    test('Then I should land on a loading page', () => {
       const html = BillsUI({
         data: [],
         loading: true
       });
       document.body.innerHTML = html;
-      expect(screen.getAllByText("Loading...")).toBeTruthy();
+      expect(screen.getAllByText('Loading...')).toBeTruthy();
     });
   });
-  describe("When I am on Bills page but back-end send an error message", () => {
-    test("Then I should land on an error page", () => {
+
+  // ERROR PAGE for views/BillsUI.js
+  describe('When I am on Bills page but back-end send an error message', () => {
+    test('Then I should land on an error page', () => {
       const html = BillsUI({
         data: [],
         loading: false,
-        error: "Whoops!"
+        error: 'Whoops!'
       });
       document.body.innerHTML = html;
-      expect(screen.getAllByText("Erreur")).toBeTruthy();
+      expect(screen.getAllByText('Erreur')).toBeTruthy();
+    });
+  });
+
+  // handleClickNewBill for container/Bills.js
+  describe('Given I am connected as Employee and I am on Bills page', () => {
+    describe('When I click on the New Bill button', () => {
+      test('Then, it should render NewBill page', () => {
+        Object.defineProperty(window, 'localStorage', {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+          'user',
+          JSON.stringify({
+            type: 'Employee',
+          })
+        );
+        const html = BillsUI({
+          data: []
+        });
+        document.body.innerHTML = html;
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({
+            pathname
+          });
+        };
+
+        const firestore = null;
+        const allBills = new Bills({
+          document,
+          onNavigate,
+          firestore,
+          localStorage: window.localStorage,
+        });
+
+        const handleClickNewBill = jest.fn(allBills.handleClickNewBill);
+        const billBtn = screen.getByTestId('btn-new-bill');
+
+        billBtn.addEventListener('click', handleClickNewBill);
+        fireEvent.click(billBtn);
+
+        expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy();
+      });
+    });
+  });
+
+  // handleClickIconEye for container/Bills.js
+  describe('Given I am connected as Employee and I am on Bills page', () => {
+    describe('When I click on the icon eye', () => {
+      test('A modal should open', () => {
+        Object.defineProperty(window, 'localStorage', {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+          'user',
+          JSON.stringify({
+            type: 'Employee',
+          })
+        );
+        const html = BillsUI({
+          data: bills
+        });
+        document.body.innerHTML = html;
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({
+            pathname
+          });
+        };
+
+        const firestore = null;
+        const allBills = new Bills({
+          document,
+          onNavigate,
+          firestore,
+          localStorage: window.localStorage,
+        });
+
+        $.fn.modal = jest.fn();
+        const eye = screen.getAllByTestId('icon-eye')[0];
+        const handleClickIconEye = jest.fn(() =>
+          allBills.handleClickIconEye(eye)
+        );
+
+        eye.addEventListener('click', handleClickIconEye);
+        fireEvent.click(eye);
+        expect(handleClickIconEye).toHaveBeenCalled();
+        const modale = document.getElementById('modaleFile');
+        expect(modale).toBeTruthy();
+      });
     });
   });
 });
